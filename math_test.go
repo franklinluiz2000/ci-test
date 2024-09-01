f761612e4ed123dc10a1e561cbe596da2ab365c3
franklinluiz2000 <franklinluiz2000@gmail.com>
2024-08-31 21:26:01 -0300

    Meu primeiro commit

---
+++ math_test.go
@@ -0,0 +1,9 @@
+import "testing"
+
+func TestSoma(t *testing.T) {
+
+	total := Soma(15, 15)
+	if total != 30 {
+		t.Errorf("O Resultado da soma é invalido: O resultado %d. Espearado: %d", total, 30)
+	}
+}
